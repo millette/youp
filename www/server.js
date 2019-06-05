5357b8d456aa7d01772d7916268181e48438a5d8
Robin Millette <robin@millette.info>
2019-06-05 13:55:19 -0400

    More debug output.

--- www/server.js
+++ www/server.js
@@ -14,7 +14,12 @@ const target = "http://localhost:3001"
 app.prepare().then(() => {
   createServer((req, res) => {
     if (req.url === "/api/profile" || req.url === "/api/login")
-      return proxy.web(req, res, { target }, console.log)
+      return proxy.web(req, res, { target }, (e) => {
+        console.log("e-errno", e.errno)
+        console.log("e.code", e.code)
+        console.log("e.address", e.address)
+        console.log("e.port", e.port)
+      })
     handle(req, res)
   }).listen(3000, (err) => {
     if (err) throw err
