f5897593b3049ff029d96ce5adf04057e6c2ea4d
Robin Millette <robin@millette.info>
2019-06-06 20:06:39 -0400

    Add debug info to server.

--- www/server.js
+++ www/server.js
@@ -13,14 +13,14 @@ const target = "http://localhost:3001"
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    if (req.url === "/api/profile" || req.url === "/api/login")
-      return proxy.web(req, res, { target }, (e) => {
-        console.log("e-errno", e.errno)
-        console.log("e.code", e.code)
-        console.log("e.address", e.address)
-        console.log("e.port", e.port)
-      })
-    handle(req, res)
+    if (req.url.indexOf("/connect/") && req.url.indexOf("/api/"))
+      return handle(req, res)
+    proxy.web(req, res, { target }, (e) => {
+      console.log("e-errno", e.errno)
+      console.log("e.code", e.code)
+      console.log("e.address", e.address)
+      console.log("e.port", e.port)
+    })
   }).listen(3000, (err) => {
     if (err) throw err
     console.log("> Ready on http://localhost:3000")
