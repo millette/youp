ef66dc41c011c3313f7874f40c8f1d5cec996a27
Robin Millette <robin@millette.info>
2019-06-06 20:06:07 -0400

    Make session cookie available client-side too.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 "use strict"
 
 // npm
+require("dotenv-safe").config()
 const Koa = require("koa")
 const session = require("koa-session") // see https://nodejs.org/api/deprecations.html#deprecations_dep0079_custom_inspection_function_on_objects_via_inspect
 const mount = require("koa-mount")
@@ -8,12 +9,17 @@ const Grant = require("grant-koa")
 
 const grant = new Grant({
   defaults: {
-    dynamic: true,
+    // dynamic: true,
     protocol: "http",
     host: "localhost:3000",
-    state: true,
-    nonce: true,
+    // state: true,
+    // nonce: true,
     transport: "session",
+  },
+  github: {
+    key: process.env.GITHUB_KEY,
+    secret: process.env.GITHUB_SECRET,
+    callback: "/",
   },
 })
 console.log("grant.config-0", grant.config)
@@ -21,7 +27,7 @@ console.log("grant.config-0", grant.config)
 const app = new Koa()
 // REQUIRED: any session store - see /examples/koa-session-stores
 app.keys = ["grant"]
-app.use(session(app))
+app.use(session({ httpOnly: false }, app))
 // mount grant
 app.use(mount(grant))
 console.log("grant.config-1", grant.config)
