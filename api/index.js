f225e0da9719cf26b55e45cc5c8777a67db66011
Robin Millette <robin@millette.info>
2019-06-06 22:42:44 -0400

    Use grant-profile.

--- api/index.js
+++ api/index.js
@@ -7,56 +7,82 @@ const bodyParser = require("koa-bodyparser")
 const session = require("koa-session") // see https://nodejs.org/api/deprecations.html#deprecations_dep0079_custom_inspection_function_on_objects_via_inspect
 const mount = require("koa-mount")
 const Grant = require("grant-koa")
+const profile = require("grant-profile").koa()
 
-const grant = new Grant({
+const callback = "/api/welcome"
+
+const grantConfig = {
   defaults: {
-    // dynamic: true,
     protocol: "http",
     host: "localhost:3000",
-    // state: true,
-    // nonce: true,
     transport: "session",
+    callback,
   },
   github: {
     key: process.env.GITHUB_KEY,
     secret: process.env.GITHUB_SECRET,
-    callback: "/",
   },
-})
+}
 
+const grant = new Grant(grantConfig)
 const app = new Koa()
-// REQUIRED: any session store - see /examples/koa-session-stores
 app.keys = ["grant"]
 app.use(session({ httpOnly: false }, app))
 app.use(bodyParser())
-
-// mount grant
 app.use(mount(grant))
+app.use(profile(grantConfig))
 
 app.use((ctx) => {
-  // console.log('ctx.session', ctx.session)
-  // ignore favicon
-  // console.log("ctx-request", ctx.request)
-  // console.log("ctx-response", ctx.response)
-  console.log("ctx-KEYS", Object.keys(ctx))
-  console.log("ctx-method", ctx.method)
+  if (ctx.request.path === callback) {
+    if (ctx.session) {
+      const {
+        profile: {
+          login,
+          id,
+          node_id,
+          avatar_url,
+          name,
+          company,
+          blog,
+          location,
+          email,
+          hireable,
+          bio,
+          public_repos,
+          public_gists,
+          followers,
+          following,
+          created_at,
+          updated_at,
+        },
+      } = ctx.session.grant
 
-  console.log("ctx-app", ctx.app)
-  console.log("ctx-originalUrl", ctx.originalUrl)
-  console.log("ctx-session", ctx.session)
-  // if (ctx.path === "/favicon.ico") return
-
-  if (ctx.path === "/api/logout") {
-    ctx.session = null
-    ctx.body = { logout: true }
+      ctx.session.grant.profile = {
+        login,
+        id,
+        node_id,
+        avatar_url,
+        name,
+        company,
+        blog,
+        location,
+        email,
+        hireable,
+        bio,
+        public_repos,
+        public_gists,
+        followers,
+        following,
+        created_at,
+        updated_at,
+      }
+    }
+    ctx.response.redirect("/")
   }
-
-  return
-  /*
-  let n = ctx.session.views || 0
-  ctx.session.views = ++n
-  ctx.body = n + " views"
-  */
+  if (ctx.request.path === "/api/logout" && ctx.request.method === "POST") {
+    ctx.session = null
+    ctx.response.redirect("/")
+  }
 })
 
 app.listen(3001)
