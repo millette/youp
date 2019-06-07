48be5da771393780ea33abe5384b916aede03c91
Robin Millette <robin@millette.info>
2019-06-06 20:13:02 -0400

    Handle session cookie.

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import nextCookie from "next-cookies"
+import cookie from "js-cookie"
+
+const atob =
+  (typeof window !== "undefined" && window.atob) ||
+  ((a) => Buffer.from(a, "base64").toString())
 
 const Home = (props) => (
   <Layout>
@@ -18,6 +24,7 @@ const Home = (props) => (
         the `/login` route.
       </li>
     </ol>
+    <pre>{JSON.stringify(props, null, "  ")}</pre>
     <style jsx>{`
       li {
         margin-bottom: 0.5rem;
@@ -25,5 +32,48 @@ const Home = (props) => (
     `}</style>
   </Layout>
 )
+
+Home.getInitialProps = (ctx) => {
+  const { req } = ctx
+  const oy = nextCookie(ctx)
+  if (oy["koa:sess"]) {
+    console.log("SESION-COOKIE!", !!req)
+    const a = atob(oy["koa:sess"])
+    const b = JSON.parse(a)
+    console.log("A", a)
+    console.log("B", b)
+    const ret = {
+      provider: b.grant.provider,
+      token: b.grant.response.access_token,
+    }
+    // cookie.set('resp', ret)
+    return ret
+  }
+  return {}
+  /*
+  console.log('oy', oy)
+  console.log('req.url', req && req.url)
+  console.log('req.headers', req && req.headers)
+  */
+
+  /*
+  if (req) return {}
+
+  const ga = cookie.get('resp')
+  console.log('GA', ga)
+  return {}
+  */
+
+  /*
+  const a2 = atob(ga)
+  const b2 = JSON.parse(a2)
+  console.log('A2', a2)
+  console.log('B2', b2)
+  return {
+    provider: b2.grant.provider,
+    token: b2.grant.response.access_token
+  }
+  */
+}
 
 export default Home
