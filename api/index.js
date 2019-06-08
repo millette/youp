595faff5f0abd22928dbe62e628f7bebde61f45a
Robin Millette <robin@millette.info>
2019-06-08 14:51:50 -0400

    Start server with broken github key and an API url to fix it.

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ const grantConfig = {
   },
   github: {
     key: process.env.GITHUB_KEY,
-    secret: process.env.GITHUB_SECRET,
+    secret: process.env.GITHUB_SECRET + "!",
   },
   twitter: {
     key: process.env.TWITTER_KEY,
@@ -37,6 +37,12 @@ app.use(mount(grant))
 app.use(profile(grantConfig))
 
 app.use((ctx) => {
+  if (ctx.request.path === "/api/fixer") {
+    console.log("grant.config", grant.config)
+    grant.config.github.secret = process.env.GITHUB_SECRET
+    ctx.body = grant.config
+  }
+
   if (ctx.request.path === callback) {
     if (ctx.session) {
       console.log("ctx.session.grant", ctx.session.grant)
