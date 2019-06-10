0cd6cfc252378562a9490ad9bad2c13ebf714d29
Robin Millette <robin@millette.info>
2019-06-10 09:01:34 -0400

    Use level for session storage.

--- api/index.js
+++ api/index.js
@@ -7,9 +7,36 @@ const bodyParser = require("koa-bodyparser")
 const session = require("koa-session") // see https://nodejs.org/api/deprecations.html#deprecations_dep0079_custom_inspection_function_on_objects_via_inspect
 const mount = require("koa-mount")
 const Grant = require("grant-koa")
+const level = require("level")
 const profile = require("grant-profile").koa()
 
 const callback = "/api/welcome"
+
+const elDb = level("my-db", { valueEncoding: "json" })
+
+const store = {
+  get: (key, maxAge, { rolling }) => {
+    console.log("get-key", key)
+    console.log("get-maxAge", maxAge)
+    console.log("get-rolling", rolling)
+    return elDb.get(key).then((x) => {
+      console.log("get-cb", JSON.stringify(x, null, "  "))
+      return x
+    })
+  },
+  set: (key, sess, maxAge, { rolling, changed }) => {
+    console.log("set-key", key)
+    console.log("set-sess", sess)
+    console.log("set-maxAge", maxAge)
+    console.log("set-rolling", rolling)
+    console.log("set-changed", changed)
+    return elDb.put(key, sess)
+  },
+  destroy: (key) => {
+    console.log("destroy-key", key)
+    return elDb.del(key)
+  },
+}
 
 const grantConfig = {
   defaults: {
@@ -18,46 +45,58 @@ const grantConfig = {
     transport: "session",
     callback,
   },
-  github: {
-    key: process.env.GITHUB_KEY,
-    secret: process.env.GITHUB_SECRET + "!",
-  },
-  twitter: {
-    key: process.env.TWITTER_KEY,
-    secret: process.env.TWITTER_SECRET,
-  },
+  github: {},
+  twitter: {},
 }
 
 const grant = new Grant(grantConfig)
 const app = new Koa()
 app.keys = ["grant"]
-app.use(session({ httpOnly: false }, app))
+// app.use(session({ httpOnly: false }, app))
+// app.use(session(app))
+app.use(session({ store, prefix: "session:" }, app))
 app.use(bodyParser())
 app.use(mount(grant))
 app.use(profile(grantConfig))
 
 app.use((ctx) => {
   if (ctx.request.path === "/api/fixer") {
-    // console.log("grant.config", grant.config)
+    console.log("grant.config", grant.config)
+    grant.config.github.key = process.env.GITHUB_KEY
     grant.config.github.secret = process.env.GITHUB_SECRET
+    console.log("grant.config", grant.config)
     ctx.body = grant.config
   }
 
   if (ctx.request.path === callback) {
     // console.log("ctx.session.grant", ctx.session && ctx.session.grant)
-    if (ctx.session) {
-      if (ctx.session.grant.response.raw.error) {
-        ctx.body = ctx.session.grant.response.raw
+    if (ctx.session && ctx.session.grant) {
+      console.log("ctx.session", ctx.session.grant)
+      // ctx.cookies.set('koa:sess', null, { sign: true })
+      // ctx.session = null
+      /*
+      const raw = ctx.session.grant.response.raw
+      if (raw.error) {
         ctx.session = null
+        // ctx.cookies.set('koa:sess')
+        ctx.cookies.set('token')
+        ctx.body = raw
         return
       }
-      ctx.session.grant.profile = ctx.session.grant.profile
+      // ctx.cookies.set('koa:sess')
+      ctx.cookies.set('token', ctx.session.grant.response.access_token, { httpOnly: false })
+      ctx.session = null
+      */
       return ctx.response.redirect("/")
     }
+    // ctx.cookies.set('koa:sess')
+    // ctx.cookies.set('token')
     ctx.body = { err: "no-session" }
   }
 
   if (ctx.request.path === "/api/logout" && ctx.request.method === "POST") {
+    // ctx.cookies.set('koa:sess')
+    // ctx.cookies.set('token')
     ctx.session = null
     ctx.response.redirect("/")
   }
