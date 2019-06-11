05b951a8da8f3a552dab7ebd853a7c7e5ebad1b5
Robin Millette <robin@millette.info>
2019-06-11 16:28:06 -0400

    Use new cookie info.

--- www/pages/index.js
+++ www/pages/index.js
@@ -5,15 +5,23 @@ import nextCookie from "next-cookies"
 // self
 import Layout from "../components/layout"
 
+/*
 const atob =
   (typeof window !== "undefined" && window.atob) ||
   ((a) => Buffer.from(a, "base64").toString())
+*/
 
-const Home = ({ token }) => (
+const Home = ({ profileId, provider, token }) => (
   <Layout token={token}>
     <h1>Cookie-based authentication example</h1>
 
-    <pre>{JSON.stringify(token, null, "  ")}</pre>
+    {token && (
+      <div>
+        <p>Profile ID: {profileId}</p>
+        <p>Provider: {provider}</p>
+        <p>Token: {token}</p>
+      </div>
+    )}
     <style jsx>{`
       li {
         margin-bottom: 0.5rem;
@@ -23,8 +31,15 @@ const Home = ({ token }) => (
 )
 
 Home.getInitialProps = (ctx) => {
-  const token = nextCookie(ctx)["token"]
-  return { token }
+  const hello = nextCookie(ctx)["hello"]
+  console.log("HELLO", new Date(), !ctx.req, hello)
+  if (!hello) return {}
+  try {
+    return JSON.parse(hello)
+  } catch (e) {
+    console.log("Cookie oups", hello, e)
+    return {}
+  }
 
   /*
   const sesh = nextCookie(ctx)["koa:sess"]
