7afcaacab1b388b377ec97dbd40f6e9e66db291e
Robin Millette <robin@millette.info>
2019-06-19 16:18:02 -0400

    Fix dev server.

--- www/server.js
+++ www/server.js
@@ -9,14 +9,14 @@ const app = next({ dev: true })
 const handle = app.getRequestHandler()
 const proxy = httpProxy.createProxyServer()
 const target = "http://localhost:3001"
-const isErr = (e) => e && handle(req, res, e)
+const isErr = (req, res, e) => e && handle(req, res, e)
 
 app.prepare().then(() => {
   createServer((req, res) => {
     if (req.url.indexOf("/connect/") && req.url.indexOf("/api/"))
       return handle(req, res)
     console.log("proxying...", new Date(), req.url)
-    proxy.web(req, res, { target }, isErr)
+    proxy.web(req, res, { target }, isErr.bind(null, req, res))
   }).listen(3000, (err) => {
     if (err) throw err
     console.log("> Ready on http://localhost:3000")
